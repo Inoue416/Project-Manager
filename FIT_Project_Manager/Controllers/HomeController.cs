3fe03f825c57e0a3a843d2e79dcab6dfa25d4a99
yuyainoue <yuyatuta@icloud.com>
2023-11-06 00:37:29 +0900

    ViewModelno

--- FIT_Project_Manager/Controllers/HomeController.cs
+++ FIT_Project_Manager/Controllers/HomeController.cs
@@ -1,6 +1,9 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FIT_Project_Manager.Models;
+// TODO: 後にSQLの処理を行うクラスを別で作りたい
+using System.Linq;
+using System.Data.SqlClient;
 
 namespace FIT_Project_Manager.Controllers;
 
@@ -15,12 +18,28 @@ public class HomeController : Controller
 
     public IActionResult Index()
     {
-        return View();
+        string db_contname = Environment.GetEnvironmentVariable("POSTGRES_DB");
+        string db_user = Environment.GetEnvironmentVariable("POSTGERS_USER");
+        string db_password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
+        // string constr = @"Data Source=192.168.0.1;Initial Catalog=iPentecSandBox;Connect Timeout=60;Persist Security Info=True;User ID=sa;Password=saPassword";
+        string sql = $@"
+            SELECT 
+                id
+                ,name
+            FROM
+                users;
+        ";
+        var viewModel = new HomeViewModel()
+        {
+            Id=0,
+            Name=db_contname
+        };
+        return View("~/Views/Home/Index.cshtml", viewModel);
     }
 
     public IActionResult Privacy()
     {
-        return View();
+        return View("~/Views/Home/Privacy.cshtml");
     }
 
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
---
+++ FIT_Project_Manager/Models/HomeViewModel.cs
@@ -0,0 +1,8 @@
+namespace FIT_Project_Manager.Models
+{
+    public sealed class HomeViewModel
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
