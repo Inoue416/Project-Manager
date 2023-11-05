97703b2330cb8393b93752c0ca4a6f2f40107f8e
Yuya Inoue <yuyatuta@icloud.com>
2023-11-05 19:12:17 +0900

    テーブルの初期化完了

--- DB/scripts/create_tables.sql
+++ DB/scripts/create_tables.sql
@@ -1,5 +1,5 @@
-CREATE TABLE IF NOT EXISTS FIT_DB.users (
-    id PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS users (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     name varchar(50) NOT NULL,
     office_id int NULL,
     user_kind int NOT NULL,
@@ -7,8 +7,8 @@ CREATE TABLE IF NOT EXISTS FIT_DB.users (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS FIT_DB.record (
-    id PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS record (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     title varchar(100) NOT NULL,
     content varchar(10000) NOT NULL,
     content_kind int NOT NULL DEFAULT 0,
@@ -16,26 +16,26 @@ CREATE TABLE IF NOT EXISTS FIT_DB.record (
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS FIT_DB.record_has (
-    id PRIMARY KEY NOT NULL,
-    foreign key(user_id) references FIT_DB.users(id) NOT NULL,
-    foreign key(record_id) references FIT_DB.record(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS record_has (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    user_id bigint references users(id) ON DELETE CASCADE,
+    record_id bigint references record(id) ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
-)
+);
 
-CREATE TABLE IF NOT EXISTS FIT_DB.office (
-    id PRIMARY KEY NOT NULL,
+CREATE TABLE IF NOT EXISTS office (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
     group_name varchar(100) NOT NULL,
     owner_id int NOT NULL,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
 
-CREATE TABLE IF NOT EXISTS FIT_DB.office_has (
-    id PRIMARY KEY NOT NULL,
-    foreign key(user_id) references FIT_DB.users(id) NOT NULL,
-    foreign key(office_id) references FIT_DB.office(id) NOT NULL,
+CREATE TABLE IF NOT EXISTS office_has (
+    id BIGSERIAL PRIMARY KEY NOT NULL,
+    user_id bigint references users(id) ON DELETE CASCADE,
+    office_id bigint references office(id) ON DELETE CASCADE,
     created_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMPTZ NOT NULL DEFAULT CURRENT_TIMESTAMP
 );
