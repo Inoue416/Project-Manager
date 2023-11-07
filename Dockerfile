6a4b5fa2439359cd75c0751bf8f2149a05477fc2
Yuya Inoue <yuyatuta@icloud.com>
2023-11-07 11:24:52 +0900

    AppコンテナとDBコンテナの通信が繋がったことを確認

--- Dockerfile
+++ Dockerfile
@@ -3,8 +3,19 @@ FROM ubuntu/nginx:latest
 RUN apt-get update
 RUN apt-get install -y dotnet-sdk-7.0
 RUN dotnet --version
+# curlを入れないとdotnetのパッケージがインストールできない。多分curlを入れることでSSLへの接続ができるようになるためだと考える
+RUN apt-get install -y curl
 
 EXPOSE 8000
 
 WORKDIR /FIT_Project_Manager
+
+# COPY ./FIT_Project_Manager /FIT_Project_Manager/
+# RUN dotnet restore --interactive
+# RUN dotnet add package Ngpsql
+# CMD [ "dotnet", "add", "package", "Npgsql" ]
+
 ENTRYPOINT [ "dotnet", "run" ]
+# ENTRYPOINT [ "dotnet", "add package Npgsql" ]
+
+# CMD [ "dotnet", "run" ]
--- FIT_Project_Manager/Controllers/HomeController.cs
+++ FIT_Project_Manager/Controllers/HomeController.cs
@@ -1,6 +1,7 @@
 ﻿using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using FIT_Project_Manager.Models;
+using System;
 // TODO: 後にSQLの処理を行うクラスを別で作りたい
 using System.Linq;
 using Npgsql;
@@ -24,45 +25,32 @@ public class HomeController : Controller
     {
         string db_contname = Environment.GetEnvironmentVariable("DB_CONTNAME");
         string db_name = Environment.GetEnvironmentVariable("POSTGRES_DB");
-        string db_user = Environment.GetEnvironmentVariable("POSTGERS_USER");
+        string db_user = Environment.GetEnvironmentVariable("POSTGRES_USER");
         string db_password = Environment.GetEnvironmentVariable("POSTGRES_PASSWORD");
         string db_port = Environment.GetEnvironmentVariable("POSTGRES_PORT");
-        string connect_query = $"Server={db_contname};Database={db_name};Port={db_port};User ID={db_user};Password={db_password};Enlist=true";
-        return connect_query;
+        var connectionString = $"Host={db_contname};Port={db_port};Username={db_user};Password={db_password};Database={db_name}";
+        // var connectionString = "Host=db;Port=5432;Username=fit_staff;Password=password;Database=fit_db";
+        // string connect_query = $"Server={db_contname}; Database={db_name}; Port={db_port}; User ID={db_user}; Password={db_password};";
+        // Console.WriteLine(connectionString2);
+        // Console.WriteLine(connectionString);
+        return connectionString;
     }
 
-    public IActionResult Index()
+    public async void GetAsyncUserData()
     {
-        string user_name;
-        int user_id;
-        using (TransactionScope ts = new TransactionScope())
-        {
-            using (NpgsqlConnection connect = new NpgsqlConnection(GetDBConnectInfo()))
-            {
-                connect.Open();
-                string cmd = $@"
-                    SELECT 
-                        id
-                        ,name
-                    FROM
-                        users
-                    WHERE
-                        id == 1;
-                ";
-                DataTable dt = new DataTable();
-                var cmd_obj = new NpgsqlCommand(cmd, connect);
-                var data = new NpgsqlDataAdapter(cmd_obj);
-                data.Fill(dt);
-                user_name = $"{dt.Rows[0][1]}";
-                user_id = (int)dt.Rows[0][0];
-            }
-            ts.Complete();
-        }
+        string connectionString = GetDBConnectInfo();
+        await using var dataSource = NpgsqlDataSource.Create(connectionString);
+        await using var connection = await dataSource.OpenConnectionAsync();
+        Console.WriteLine("Success...");
+    }
 
+    public async Task<IActionResult> Index()
+    {
+        GetAsyncUserData();   
         var viewModel = new HomeViewModel()
         {
-            Id=user_id,
-            Name=user_name
+            Id=0,
+            Name="sample"
         };
         return View("~/Views/Home/Index.cshtml", viewModel);
     }
