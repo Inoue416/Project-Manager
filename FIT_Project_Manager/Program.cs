37d3eb8cd3ab89b73b214e28980905523a04607c
Yuya Inoue <yuyatuta@icloud.com>
2023-11-08 18:15:53 +0900

    Sessionをミドルフェアで有効にするための設定を記述

--- FIT_Project_Manager/Program.cs
+++ FIT_Project_Manager/Program.cs
@@ -4,6 +4,15 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 
+builder.Services.AddDistributedMemoryCache();
+
+builder.Services.AddSession(options =>
+{
+    options.IdleTimeout = TimeSpan.FromSeconds(10);
+    options.Cookie.HttpOnly = true;
+    options.Cookie.IsEssential = true;
+});
+
 var app = builder.Build();
 
 
@@ -23,6 +32,8 @@ app.UseRouting();
 
 
 app.UseAuthorization();
+
+app.UseSession();
 
 app.MapControllerRoute(
     name: "default",
